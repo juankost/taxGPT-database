56ea71fa764726c6b810a2b394181b93ade5be7b
juankost <juan.kostelec@gmail.com>
2024-02-21 08:49:32 +0100

    relative import error

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Run data pipeline
-python3 ./app/pipeline/data_pipeline.py
+python3 -m app.pipeline.data_pipeline
 
 # Then start the main application
-exec python3 ./app/app.py
+# exec python3 ./app/app.py
+exec python3 -m app.app
