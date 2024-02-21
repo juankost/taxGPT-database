6689426bc50168d9f62bd0a2edf8ab78d2c0743e
juankost <juan.kostelec@gmail.com>
2024-02-21 07:55:03 +0100

    fixing python packages installation

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
+# Read requirements.txt and use it for the install_requires option
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="taxgpt-database",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        # list your project's dependencies here
-    ],
+    install_requires=requirements,
     # Additional metadata about your package
 )
